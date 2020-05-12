b5f2be69a5f38316c95f33fa5fb9847b4f70049a
Marbeez <martin.kirpu@gmail.com>
2020-05-12 17:47:42 +0300

    Update kirpi.py

--- kirpi.py
+++ kirpi.py
@@ -1,5 +1,23 @@
-insertpls = str(input("Sisesta oma diagnoos: "))
-if insertpls == "Autism":
-    print("Tore")
-elif insertpls == "OCD":
-    print("WOW")
+from urllib.request import urlopen
+edetabel = urlopen("https://ratings.fide.com/top.phtml?list=men")
+baidid = edetabel.read()
+tekst = [baidid.decode()]
+
+def otsib():
+    a = 0
+    while (str(tekst)[0+a:i+a] == kogu_array) == False:
+        a += 1
+    print("aha")
+
+kogu_array = []
+
+eesnimi = str(input("Sisestage malemängja eesnimi: ")).title()
+perenimi = str(input("Sisestage malemängja perekonnanimi: ")).title()
+
+kogu_array.append(perenimi + ", " + eesnimi)
+for i in kogu_array:
+    i = len(i)
+
+kogu_array = str(kogu_array)[2:-2]
+
+otsib()
