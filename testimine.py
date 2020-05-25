f0a62870a6921257d0960fa4e779e2de0f009ec4
Marbeez <martin.kirpu@gmail.com>
2020-05-25 17:59:04 +0300

    e

--- testimine.py
+++ testimine.py
@@ -26,10 +26,6 @@ if valikubox == "Nime järgi":
         tekstarray.append("Mängija koht edetabelis on: " + str(rank))
         return tekstarray
 
-    edetabel = urlopen("https://ratings.fide.com/top.phtml?list=men")
-    baidid = edetabel.read()
-    tekst = baidid.decode()
-
     nimearr = ["Eesnimi", "Perekonnanimi"]
     nimed = multenterbox("Sisestage mängija ees- ja perekonnanimi", "Mängija otsija 3000", nimearr)
 
@@ -46,8 +42,8 @@ if valikubox == "Nime järgi":
             i += 1
 
     msgbox(infotekst)
-
     #easygui täiendatud infot lisaks kursusematerjalile saime siit: http://easygui.sourceforge.net/tutorial.html
+
 else:
     asukohtgui = enterbox("Sisestage malemängja asukoht edetabelis", "Mängija otsija 3000")
     asukoht = asukohtgui.title()
@@ -60,6 +56,10 @@ else:
 
     info = tekst.index(nimi) + 53 + len(nimi)  
     elo = tekst[info:info+4]
+    m2ngud = tekst[info+19:info+24]
+    m2ngud = m2ngud.replace("<", "").replace("/", "").replace("t", "").replace("d", "")
+    synniaasta = tekst[info+35:info+39]
+    riik = tekst[info-18:info-15]
 
     i = 9266
     asukoht = 0
@@ -80,6 +80,9 @@ else:
     infojärjend = ['Mängija nimi (eesnimi- ja perekonnanimi): ' + täisnimi]
     infojärjend.append('Elo reiting: ' + elo)
     infojärjend.append('Asukoht FIDE top 100 mängijate edetabelis: ' + str(asukoht) + '. kohal')
+    infojärjend.append('Mängija mängis ' + str(m2ngud) + ' mängu see kuu')
+    infojärjend.append('Mängija sündis ' + str(synniaasta) + ' aastal')
+    infojärjend.append('Mängija on ' + str(riik) + ' riigi esindaja')
     koguinfo = ""
     i = 0
     for line in infojärjend:
